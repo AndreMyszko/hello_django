fd840eeb83d3c72126c377494b26e4ae99ce3156
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2022-08-02 23:37:26 -0300

    create. login logout auth

--- core/views.py
+++ core/views.py
@@ -1,25 +1,38 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from core.models import Evento
-HttpRequest
 
 # Create your views here.
 
 
-def index(request):
-    return redirect('/agenda/')
+def login_user(request):
+    return render(request, 'login.html')
 
 
-def user(request, firstname, lastname, age):
-    return HttpResponse(
-        '<h2>hello world</h2>'
-        '<br> be wolcome'
-        '<br> name: <b>{} {}</b>'
-        '<br> age: <b>{}</b>'
-        .format(firstname, lastname, age))
+def logout_user(request):
+    logout(request)
+    return redirect('/')
 
 
+def login_submit(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'usuário ou senha inválidos')
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_agendamentos(request):
-    evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
     data = {'eventos': evento}
     return render(request, 'agenda.html', data)
