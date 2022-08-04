d0ffa1fe9b26c8427fb61fac0d1691ca3df905c1
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2022-08-04 14:05:09 -0300

    create. json integration

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -27,9 +27,13 @@ urlpatterns = [
     path('login/submit', views.login_submit),
     path('logout/', views.logout_user),
 
-    path('agenda/', views.lista_agendamentos),
+    path('agenda/', views.lista_eventos),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
     path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
+
+    # JSON
+    path('agenda/json/evento', views.json_lista_evento),
+    path('agenda/json/evento/<int:id_usuario>', views.json_lista_evento_id),
 
 ]
